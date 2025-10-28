a04deb3a0318cdfe7ff45100befad406fb4c348b
jackaranaram <104057685+jackaranaram@users.noreply.github.com>
2025-10-28 18:12:03 -0500

    feat: add login and register pages

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/LogIn";
+import Register from "./pages/Register";
+import RecoverPassword from "./pages/RecoverPassword";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,15 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/login", element: <Login />
+      },
+      {
+        path: "/register", element: <Register />
+      },
+      {
+        path: "/recover-password", element: <RecoverPassword />
       }
     ],
   },
